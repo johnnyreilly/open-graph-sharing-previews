184809b8d24a107a6d8dbadcf7e607ae157dcac9
johnnyreilly <johnny_reilly@hotmail.com>
2021-11-07 15:06:57 +0000

    tweak

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,9 +4,9 @@ import "./app.css";
 
 function App() {
   const openGraphData = {
-    title: "Sharing previews",
+    title: "Open Graph: a guide to sharing previews",
     description:
-      "This is a page that demonstrates sharing previews using the Open Graph protocol",
+      "This page features the Open Graph protocol markup for sharing previews.",
     url: "https://johnnyreilly.github.io/open-graph-sharing-previews/",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/7/72/Open_Graph_protocol_logo.png",
@@ -20,9 +20,7 @@ function App() {
         <meta property="og:image" content={openGraphData.image} />
         <meta name="twitter:card" content="summary" />
       </Head>
-      <h1>
-        This page features the Open Graph protocol markup for sharing previews.
-      </h1>
+      <h1>{openGraphData.title}</h1>
       <img src={openGraphData.image} alt="The Open Graph protocol logo" />
       <h2>Share it and see!</h2>
     </div>
